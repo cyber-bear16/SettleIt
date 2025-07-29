059e9d39bab635f95bf5c6e16b96ae081fccc41e
Abhimanyu Chauhan <chauhanabhimanyu833@gmail.com>
2025-07-29 20:12:48 +0530

    First Commit

---
+++ src/main/java/com/settleit/SettleIt/SettleItApplication.java
@@ -0,0 +1,13 @@
+package com.settleit.SettleIt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SettleItApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SettleItApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/settleit/SettleIt/SettleItApplicationTests.java
@@ -0,0 +1,13 @@
+package com.settleit.SettleIt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SettleItApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
